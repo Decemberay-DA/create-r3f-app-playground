f9385a3e5900f9f285988c73d6bb787f55e4ca82
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-05-08 01:11:21 +0300

    sacroll

---
+++ src/FExport.ts
@@ -0,0 +1,7 @@
+export * as THREE from "three"
+export * as FIBER from "@react-three/fiber"
+export * as DREI from "@react-three/drei"
+export * as LAINS from "@studio-freight/lenis"
+
+import * as React from "react"
+export { React as REACT }
--- src/templates/Shader/Shader.tsx
+++ src/templates/Shader/Shader.tsx
@@ -1,30 +1,30 @@
 // @ts-nocheck
-import * as THREE from 'three'
-import { extend, useFrame } from '@react-three/fiber'
-import { shaderMaterial } from '@react-three/drei'
-import vertex from './glsl/shader.vert'
-import fragment from './glsl/shader.frag'
-import { forwardRef, useImperativeHandle, useRef } from 'react'
+import * as THREE from "three"
+import vertex from "./glsl/shader.vert"
+import fragment from "./glsl/shader.frag"
+import { FIBER } from "@/FExport"
+import { REACT } from "@/FExport"
+import { DREI } from "@/FExport"
 
-const ShaderImpl = shaderMaterial(
-  {
-    time: 0,
-    color: new THREE.Color(0.05, 0.0, 0.025),
-  },
-  vertex,
-  fragment,
+const ShaderImpl = DREI.shaderMaterial(
+	{
+		time: 0,
+		color: new THREE.Color(0.05, 0.0, 0.025),
+	},
+	vertex,
+	fragment,
 )
 
-extend({ ShaderImpl })
+FIBER.extend({ ShaderImpl })
 
 // eslint-disable-next-line react/display-name
-const Shader = forwardRef(({ children, ...props }, ref) => {
-  const localRef = useRef()
+const Shader = REACT.forwardRef(({ children, ...props }, ref) => {
+	const localRef = REACT.useRef()
 
-  useImperativeHandle(ref, () => localRef.current)
+	REACT.useImperativeHandle(ref, () => localRef.current)
 
-  useFrame((_, delta) => (localRef.current.time += delta))
-  return <shaderImpl ref={localRef} glsl={THREE.GLSL3} {...props} attach='material' />
+	FIBER.useFrame((_, delta) => (localRef.current.time += delta))
+	return <shaderImpl ref={localRef} glsl={THREE.GLSL3} {...props} attach='material' />
 })
 
 export default Shader
