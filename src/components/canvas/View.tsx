d187782c01019b665f80d295f87d3531ce034ab9
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-05-08 02:57:02 +0300

    f

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -4,9 +4,7 @@ import { forwardRef, Suspense, useImperativeHandle, useRef } from "react"
 import { OrbitControls, PerspectiveCamera, View as ViewImpl } from "@react-three/drei"
 import { Three } from "@/helpers/components/Three"
 import { THREE } from "@/FExport"
-import { THREE } from "@/FExport"
 
-export const Common = ({ color }: { color: THREE.ColorRepresentation }) => (
 export const Common = ({ color }: { color: THREE.ColorRepresentation }) => (
 	<Suspense fallback={null}>
 		{color && <color attach='background' args={[color]} />}
@@ -23,31 +21,18 @@ type ViewProps = {
 	[key: string]: any
 }
 
-<<<<<<< HEAD
 const View = forwardRef(({ children, orbit, ...props }: ViewProps, ref) => {
 	const localRef = useRef<HTMLDivElement>(null)
 	useImperativeHandle(ref, () => localRef.current)
-=======
-const View = forwardRef<HTMLDivElement, ViewProps>((props, ref) => {
-	const localRef = useRef<HTMLDivElement>(null)
-	useImperativeHandle(ref, () => localRef.current!)
->>>>>>> a63f69a31fbc2dc2ac5f442473827b5971435b63
 
 	return (
 		<>
 			<div ref={localRef} {...props.otherProps} />
 			<div ref={localRef} {...props.otherProps} />
 			<Three>
-<<<<<<< HEAD
 				<ViewImpl track={localRef as React.MutableRefObject<HTMLDivElement>}>
 					{children}
 					{orbit && <OrbitControls />}
-=======
-				<ViewImpl track={localRef as React.MutableRefObject<HTMLElement>}>
-					{" "}
-					{props.children}
-					{props.orbit && <OrbitControls />}
->>>>>>> a63f69a31fbc2dc2ac5f442473827b5971435b63
 				</ViewImpl>
 			</Three>
 		</>
